82f6743fe596f1575af5a624c3722c3f01a42b52
Ronnie Huggins <92937892+RonnieJr21@users.noreply.github.com>
2023-07-19 16:17:14 -0400

    Update providers.tf

--- AWS/design-1/providers.tf
+++ AWS/design-1/providers.tf
@@ -1,1 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA3R4CVWRS4R5U6P2T"
+  secret_key = "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"
+}
