6e17956a3212462eef79748a0984ced9e108991e
Ronnie Huggins <92937892+RonnieJr21@users.noreply.github.com>
2023-07-19 16:21:52 -0400

    Update providers.tf
    saftey

--- AWS/design-1/providers.tf
+++ AWS/design-1/providers.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA3R4CVWRS4R5U6P2T"
-  secret_key = "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"
+  access_key = "access"
+  secret_key = "secret"
 }
